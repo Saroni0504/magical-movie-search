ba5015952d6e76031bd5a94a8c05ff45b3d25ea6
roni satchi <roni.satchi@gmail.com>
2024-10-28 23:19:35 +0200

    enriching dataset

--- data/constants.py
+++ data/constants.py
@@ -3,9 +3,9 @@ DATASET_NAME = "disney_movies_dataset"
 DATASET_PATH = "data"
 MOVIE_INFOBOX_HEADERS = [
     ("Running time", "str"),
+    ("Starring", "list"),
     ("Budget", "str"),
     ("Box office", "str"),
-    ("Starring", "list"),
 ]
 OPENAI_FUNCTION_PARAMETERS = ["movie_summary", "tags", "genre"]
 WIKITABLE_MOVIE_COLUMNS = ["Release date", "Title", "Notes"]
--- data/utils.py
+++ data/utils.py
@@ -1,5 +1,5 @@
-
 import random
+import re
 import time
 
 import requests
@@ -41,3 +41,56 @@ def image_exists_in_dir(image_name: str, dir_name: str) -> bool:
     # The glob module finds all the pathnames matching a specified pattern
     files = list(directory.glob(f"{image_name}.*"))
     return bool(files)
+
+
+def infobox_number_parser(formatted_number: str) -> int:
+    if not isinstance(formatted_number, str):
+        return None
+
+    cleaned_text = re.sub(r"[\[\(].*?[\]\)]", "", formatted_number)
+
+    pattern = (
+     r"(?:\$|US\$)\s*(\d{1,3}(?:,\d{3})*(?:\.\d+)?)(?:\s*[-–—to]*\s*"
+     r"(?:(?:\$|US\$)?\s*(\d{1,3}(?:,\d{3})*(?:\.\d+)?)))?\s?"
+     r"(million|billion)?"
+    )
+    matches = re.findall(pattern, cleaned_text)
+
+    UNIT_MULTIPLIERS = {
+        "million": 1_000_000,
+        "billion": 1_000_000_000,
+    }
+
+    total_sum = 0
+    is_valid_string = False
+    for match in matches:
+        number1_str, number2_str, unit = match
+
+        number1 = float(number1_str.replace(",", ""))
+        if unit.lower().strip() in UNIT_MULTIPLIERS:
+            number1 *= UNIT_MULTIPLIERS[unit.lower().strip()]
+        number = number1
+
+        if number2_str:
+            number2 = float(number2_str.replace(",", ""))
+            if unit.lower().strip() in UNIT_MULTIPLIERS:
+                number2 *= UNIT_MULTIPLIERS[unit.lower().strip()]
+            number = (number1 + number2) / 2
+
+        total_sum += number
+        is_valid_string = True
+
+    if not is_valid_string:
+        return None
+    return int(round(total_sum))
+
+
+def formatted_running_time(running_time):
+    if not isinstance(running_time, str):
+        return None
+    running_time = running_time.split()
+    running_time = running_time[0]
+    if "–" in running_time:
+        running_time = running_time.split("–")
+        running_time = (float(running_time[0]) + float(running_time[1])) // 2
+    return int(running_time)
